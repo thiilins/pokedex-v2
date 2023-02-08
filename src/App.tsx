8d86124856d9934f94030dd4d99a44dece658bf1
Thiago Lins <falecom@thiagolins.dev.br>
2023-02-07 21:16:00 -0300

    [Update]

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import Routes from '@/routes'
 import GlobalStyles from '@styles/global'
-import ToastContainer from '@components/ToastContainer'
 import theme from '@/styles/theme'
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <React.StrictMode>
         <GlobalStyles />
-        <ToastContainer />
         <Routes />
       </React.StrictMode>
     </ThemeProvider>
